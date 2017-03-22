ee4178538b086d72435c16736e5b065675cbcc0d
josephd <josephd@lab1-40.eng.utah.edu>
2017-03-21 19:23:31 -0600

    Table update change.

--- src/phase2/User.java
+++ src/phase2/User.java
@@ -13,7 +13,7 @@ public class User
 	* This inserts a new user into the user table.
 	* It will throw an exception if the login is the same as another user.
 	*/
-	public void InsertUser(String Name, String Login, int type, String Password, String Address, int PhoneNumber, Connector con) throws Exception
+	public void InsertUser(String Name, String Login, int type, String Password, String Address, String PhoneNumber, Connector con) throws Exception
 	{
 		//INSERT INTO Users (Name, Login, userType, Password, Address, PhoneNumber) VALUES ('Joey Despain','Jdlog','0','password','123 street','5556666');
 		String query;
@@ -27,7 +27,7 @@ public class User
 		      preparedStmt.setInt(3, type);
 		      preparedStmt.setString(4, Password);
 		      preparedStmt.setString(5, Address);
-		      preparedStmt.setInt(6, PhoneNumber);
+		      preparedStmt.setString(6, PhoneNumber);
 		      preparedStmt.execute();
 			 
         } catch(Exception e) {
@@ -60,8 +60,8 @@ public class User
 		System.out.print("Type in your address then return.");
 		String address = scan.next();
 		
-		System.out.println("\nType in your phonenumber then return.");
-		int number = scan.nextInt();
+		System.out.print("\nType in your phonenumber then return\n");
+		String number = scan.next();
 		
 		try
 		{
